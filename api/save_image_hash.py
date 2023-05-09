84db9d077551d8aa9fb6382a84dbe7dac7531a76
Lehti <leaf.mmo@gmail.com>
2023-05-09 12:49:40 +0900

    add- G14

--- api/save_image_hash.py
+++ api/save_image_hash.py
@@ -4,33 +4,37 @@ import imagehash
 import json
 from concurrent.futures import ProcessPoolExecutor
 
-image_dir  = "map_image/G15/"
+image_dir = "map_image/G14/"
+
 
 # image_hashの取得
 def get_image_hash(image_path):
-  if os.path.isfile(image_path):
-    return imagehash.average_hash(Image.open(image_path))
-  else:
-    return None
+    if os.path.isfile(image_path):
+        return imagehash.average_hash(Image.open(image_path))
+    else:
+        return None
 
+
+image_name_list = ["Garlemald", "Labyrinthos", "MareLamentorum", "Thavnair", "UltimaThule"]
 
 # main関数
 def main():
-  items = []
-  with ProcessPoolExecutor(max_workers=4) as executor:
-      for i in range(1,9):
-        image_path = image_dir + "Elpis" + str(i) + ".webp"
+    items = []
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        for image_name in image_name_list:
+            for i in range(1, 9):
+                image_path = image_dir + image_name + str(i) + ".webp"
 
-        future = executor.submit(get_image_hash, image_path)
-        img_hash = future.result()
-        
-        if img_hash is not None:
-            items.append({"hash": str(img_hash), "name": "Elpis" + str(i) + ".webp"})
+                future = executor.submit(get_image_hash, image_path)
+                img_hash = future.result()
 
-  with open("map_hash/image_hash_G15.json", "w", encoding="utf-8") as f:
-      json.dump(items, f, ensure_ascii=False)
+                if img_hash is not None:
+                    items.append({"hash": str(img_hash), "name": image_name + str(i) + ".wpbp"})
+
+    with open("map_hash/image_hash_G14.json", "w", encoding="utf-8") as f:
+        json.dump(items, f, ensure_ascii=False)
 
 
 # 実行
 if __name__ == "__main__":
-  main()
+    main()
--- api/similarity.py
+++ api/similarity.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, UploadFile, status, Request, Body
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
 
 from pillow_heif import register_heif_opener
 from PIL import Image
@@ -37,15 +36,15 @@ def compare_image_hash(input_image_hash, image_hash):
     return input_image_hash - imagehash.hex_to_hash(image_hash)
 
 
-@app.post("/uploadfile/{select}")
+@app.post("/ff14_map_treasure_uploadfile/{select}")
 async def upload_file(upload_file: UploadFile, select: str):
     upload_file.file.seek(0, 2)
-    if upload_file.file.tell() > 2097152:
-        return JSONResponse(content={"error_message": "ファイルサイズは2Mbまでアップロードできます。"}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    if upload_file.file.tell() > 1:
+        return JSONResponse(content={"error_message": "ファイルサイズは10Mbまでアップロードできます。"}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-    if ("image/" not in upload_file.content_type) or (upload_file.content_type != "application/octet-stream"):
-        if "image/" not in mimetypes.guess_type(upload_file.filename)[0]:
-            return JSONResponse(content={"error_message": "画像のみアップロードできます。"}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    # if ("image/" not in upload_file.content_type) or (upload_file.content_type != "application/octet-stream"):
+    #     if "image/" not in mimetypes.guess_type(upload_file.filename)[0]:
+    #         return JSONResponse(content={"error_message": "画像のみアップロードできます。"}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     register_heif_opener()
     input_image_hash = imagehash.average_hash(Image.open(upload_file.file))
@@ -53,21 +52,11 @@ async def upload_file(upload_file: UploadFile, select: str):
     with open(f"map_hash/image_hash_{select}.json", "r", encoding="utf-8") as f:
         image_hash_list = json.load(f)
 
-    min = None
-    first = None
-    second = None
+    diff_dict = {}
     with ProcessPoolExecutor(max_workers=4) as executor:
         for image_hash in image_hash_list:
             future = executor.submit(compare_image_hash, input_image_hash, image_hash["hash"])
-            diff = future.result()
-
-            if min is None:
-                min = diff
-                first = image_hash["name"]
-            if min > diff:
-                min = diff
-                second = first
-                first = image_hash["name"]
+            diff_dict[image_hash["name"]] = future.result()
         executor.shutdown()
 
         # 座標JSON取得
@@ -75,9 +64,13 @@ async def upload_file(upload_file: UploadFile, select: str):
         with open(f"map_place/map_place_{select}.json", "rb") as f:
             place = json.load(f)
 
-        result = [first, second]
+        # 昇順並び替え及び小さい値2個取得
+        sorted_diff_dict = sorted(diff_dict.items(), key=lambda x:x[1])
+        sorted_diff_dict = sorted_diff_dict[0:2]
+        sorted_diff_dict = dict((x, y) for x, y in sorted_diff_dict) 
+        
         image_base64 = []
-        for name in result:
+        for name in sorted_diff_dict.keys():
             if name is None:
                 continue
 
@@ -86,7 +79,7 @@ async def upload_file(upload_file: UploadFile, select: str):
                 image_base64.append(data)
 
             #  全体図
-            whole_image_name = re.sub("\d", "", first)
+            whole_image_name = re.sub("\d", "", name)
             with open(f"map_image/{select}/{whole_image_name}", "rb") as image_file:
                 data = base64.b64encode(image_file.read()).decode("utf-8")
                 image_base64.append(data)
